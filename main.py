dad23517afe5a43f56f9ae1d39d9b670d22808e4
MatthewPN2004J <matthew_ng_wei_zhe@connect.ite.edu.sg>
2021-02-16 05:50:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+#########################################################################
+#Title: PYTHON Project Scenario - Data Analysis
+#Description: This program allows user to analyse.......
+#Name: <Matthew Ng Wei Zhe>
+#Group Name: <...>
+#Class: <...>
+#Date: <...>
+#Version: <...>
+#########################################################################
+
+#########################################################################
+#IMPORT Pandas Library for Data Analysis
+#########################################################################
+#import pandas for data analysis
+import pandas as pd
+#########################################################################
+#IMPORT Pandas Library for Data Analysis
+#########################################################################
+
+#########################################################################
+#CLASS Branch - Data Analysis
+#load excel data (CSV format) to dataframe
+#########################################################################
+class DataAnalysis:
+  def __init__(self):
+
+    #load excel data (CSV format) to dataframe - 'df'
+    dataframe = pd.read_csv('MonthyVisitors.csv')
+
+    #show specific country dataframe
+    sortCountry(dataframe)
+#########################################################################
+#CLASS Branch: End of Code
+#########################################################################
+
+#########################################################################
+#FUNCTION Branch - sortCountry
+#parses data and displays sorted result(s)
+#########################################################################
+def sortCountry(df):
+
+  #print number of rows in dataframe
+  print("There are " + str(len(df)) + " data rows read from the MonthlyVisitors.csv file. \n")
+
+  #slicing dataframe using iloc 2007 - 2017 SEA to form new dataframe
+  new_df = df.iloc[348:479, 0:9]
+
+  #disply new dataframe (rows and colums)
+  print("The following new dataframe for SEA from 2007 - 2017 are read as follows: \n")
+  print(new_df)
+
+#########################################################################
+#FUNCTION Branch: End of Code
+#########################################################################
+
+#########################################################################
+#Main Branch
+#########################################################################
+if __name__ == '__main__':
+  
+  #Project Title
+  print('######################################')
+  print('# Data Analysis App - PYTHON Project #')
+  print('######################################')
+
+  #perform data analysis on specific excel (CSV) file
+  DataAnalysis()
+
+#########################################################################
+#Main Branch: End of Code
+#########################################################################
