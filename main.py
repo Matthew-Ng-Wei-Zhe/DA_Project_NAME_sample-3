1bb0f9e43ae0b39a6e735627fba123a8500401da
MatthewPN2004J <matthew_ng_wei_zhe@connect.ite.edu.sg>
2021-02-17 07:01:54 +0000

    Version 1.1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 #Group Name: <Python Underdog>
 #Class: <PN2004J>
 #Date: <17-02-21>
-#Version: <1.0>
+#Version: <1.1>
 #########################################################################
 
 #########################################################################
@@ -13,6 +13,10 @@
 #########################################################################
 #import pandas for data analysis
 import pandas as pd
+
+#import matplotlib for graphic mode (Optional)
+import matplotlib.pyplot as pit
+
 #########################################################################
 #IMPORT Pandas Library for Data Analysis
 #########################################################################
@@ -38,9 +42,7 @@ class DataAnalysis:
 #parses data and displays sorted result(s)
 #########################################################################
 def sortCountry(df):
-
-  #print number of rows in dataframe
-  print("There are " + str(len(df)) + " data rows read from the MonthlyVisitors.csv file. \n")
+  token = 0
 
   #slicing dataframe using iloc 2007 - 2017 SEA to form new dataframe
   new_df = df.iloc[348:479, 0:9]
@@ -48,7 +50,7 @@ def sortCountry(df):
   new_df = new_df.reset_index(drop=True)
 
   #disply new dataframe (rows and colums)
-  print("The following new dataframe for SEA from 2007 - 2017 are read as follows: \n")
+  print("\nThe following dataframe for SEA from 2007 - 2017 are read as follows: \n")
   print(new_df)
 
   #selecting country columns and adding them on a vertical axis in descending order based on top 3
@@ -60,12 +62,37 @@ def sortCountry(df):
   #reset index to generate new dataframe for sum value output
   top3_country = top3_country.reset_index()
   #sort sum value output into columns based on country and visitors
-  top3_country.columns = ['Country', 'Visitors']
+  top3_country.columns = ["Country", "Visitors"]
   #label top 3
   top3_country.index = ["Most Visted →", "Second Most Visted → ", "Third Most Visted →"]
 
   #display top 3 countries coming to Singapore in SEA from 2007 - 2017
-  print(top3_country)  
+  try:
+   print(top3_country)
+  except:
+   print("An ERROR has occured!!!")
+  else:
+    token = token + 1
+
+  if token >= 1:
+    user_decision = input("\nDo you want a graphical model? Yes/No : ",)
+    if user_decision == "Yes":
+     activities = ['Indonesia', 'Malaysia', 'Philippines']
+     slices = [1, 3, 4]
+     pit.pie(slices,
+        labels=activities,
+        startangle=90,
+        shadow=True,
+        explode=(0.2, 0, 0),
+        autopct='%1.2f%%')
+
+     pit.show()
+     print("Request is successful!")
+    elif user_decision == "No":
+     print("Request has been passed!")
+    else:
+      print("Invalid input! Request has been passed!")
+
 
   #return function to caller which makes call to other function
   return
